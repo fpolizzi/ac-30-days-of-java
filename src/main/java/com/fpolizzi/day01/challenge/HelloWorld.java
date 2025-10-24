2bdbc82d2fcfc3c28b80c131be2e3fe499c71ae7
Franco Polizzi <me@fpolizzi.com>
2025-10-25 00:55:54 +0200

    add day01 challenges

---
+++ src/main/java/com/fpolizzi/day01/challenge/HelloWorld.java
@@ -0,0 +1,10 @@
+package com.fpolizzi.day01.challenge;
+
+/**
+ * Created by fpolizzi on 25.10.25
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, AmigosCode!");
+    }
+}
